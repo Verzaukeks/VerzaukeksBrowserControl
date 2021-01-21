c0c7db64171652ee7d8f1065bf66148c4750a03b
Verzaukeks <verzaukeks@gmail.com>
2021-01-21 14:10:11 +0100

    README update

--- README.md
+++ README.md
@@ -7,12 +7,10 @@ and get access to many useful features as if it were an add-on.
 Download Chrome-Addon Here (not available / unstable and buggy)
 
 ![Firefox Popup](addon/extras/screenshot_popup.png)
-
-### Dependencies
-* [gson](https://github.com/google/gson)
+![Firefox Popup Dark](addon/extras/screenshot_popup_dark.png)
 
 ### Kotlin Usage
-```kotlin
+````kotlin
 val browser = Browser()
 browser.start() // browser.start(port)
 
@@ -39,4 +37,39 @@ tab.remove()
 
 
 browser.stop() // stop(clearListeners)
-```
+````
+#### Dependencies
+* [gson](https://github.com/google/gson)
+
+### Python Usage
+````python
+from browser import Browser
+from time import sleep
+
+browser = Browser()
+browser.start()  # browser.start(port)
+browser.ping()
+
+tab = browser.new_tab('https://example.com')  # new_tab(url, active)
+current_tab = browser.get_current_tab()
+tabs = browser.get_tabs()  # get_tabs(options)
+
+browser.on_tab_created.append(lambda it: print(f'New tab was created: {it}'))
+browser.on_tab_updated.append(lambda it: print(f'A tab was updated: {it}'))
+
+result = tab.execute_script('console.log(\'hello\'); \'result value\';')  # execute_script(script, expect_answer)
+tab.insert_css('* {background: red}')
+
+tab.click_element('button')  # click_element(selector, wait_till_finished)
+tab.input_text('input', 'custom text here')  # input_text(selector, value, wait_till_finished)
+
+element = tab.query_selector('h1')
+elements = tab.query_selector_all('div')
+
+tab.update_info()
+tab.reload()  # tab.reload(bypass_cache)
+tab.remove()
+
+sleep(1)
+browser.stop()  # stop(clear_listeners)
+````
--- python/src/test.py
+++ python/src/test.py
@@ -1,35 +1,30 @@
 from browser import Browser
 from time import sleep
 
-import json
-
 if __name__ == "__main__":
-
     browser = Browser()
-    browser.start() # browser.start(port)
+    browser.start()  # browser.start(port)
     browser.ping()
 
-    tab = browser.new_tab('https://example.com') # new_tab(url, active)
+    tab = browser.new_tab('https://example.com')  # new_tab(url, active)
     current_tab = browser.get_current_tab()
-    tabs = browser.get_tabs() # get_tabs(options)
+    tabs = browser.get_tabs()  # get_tabs(options)
 
-    browser.on_tab_created.append(lambda tab: print(f'New tab was created: {tab}'))
-    browser.on_tab_updated.append(lambda tab: print(f'A tab was updated: {tab}'))
+    browser.on_tab_created.append(lambda it: print(f'New tab was created: {it}'))
+    browser.on_tab_updated.append(lambda it: print(f'A tab was updated: {it}'))
 
-
-    result = tab.execute_script('console.log(\'hello\'); \'result value\';') # execute_script(script, expect_answer)
+    result = tab.execute_script('console.log(\'hello\'); \'result value\';')  # execute_script(script, expect_answer)
     tab.insert_css('* {background: red}')
 
-    tab.click_element('button') # click_element(selector, wait_till_finished)
-    tab.input_text('input', 'custom text here') # input_text(selector, value, wait_till_finished)
+    tab.click_element('button')  # click_element(selector, wait_till_finished)
+    tab.input_text('input', 'custom text here')  # input_text(selector, value, wait_till_finished)
 
     element = tab.query_selector('h1')
     elements = tab.query_selector_all('div')
 
     tab.update_info()
-    tab.reload() # tab.reload(bypass_cache)
+    tab.reload()  # tab.reload(bypass_cache)
     tab.remove()
 
-    
     sleep(1)
-    browser.stop() # stop(clear_listeners)
+    browser.stop()  # stop(clear_listeners)
